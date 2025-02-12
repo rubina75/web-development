58963a1751348de49713dad2fd0be19ab7037852
rubina75 <www.khanruby49@gmail.com>
2025-02-12 16:31:05 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+let a = 5
+var name = "rubi"
+const pi = 3.142
+// console.log(name)
+// console.log(a)
+name= "tehreem"
+a = 8
+console.log(a)
+console.log(name)
+let name1 ="aa"
+let MyName ="java script"
+let isLinkedIN ="TRUE"
+let email;
+console.log("The type of email is:", typeof email);
+let text=""
+let textNum=Boolean(text)
+console.log(typeof textNum);
+console.log(textNum);
+
+console.log(3<5);
+console.log(3>5);
+console.log(5=="5");
+console.log(5==="5");
+
+// let radius = 3
+// const pi = 3.142
+// let area = pi*(radius**2)
+// console.log(area);
+
+let radius = 3
+// const pi = 3.142
+let area = pi*(radius**2)
+console.log(area);
+
+if(2>4){
+    console.log('hello');
+}
+else{
+    console.log('bye');
+}
