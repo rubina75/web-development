afd21fd02fbc2634df57feedb6520d3acb3da4c6
rubina75 <www.khanruby49@gmail.com>
2025-02-20 17:30:43 +0500

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,44 @@
+// let shoppinglist=["rubina","banana","strawberry","apple","peaches","carrots","tomatos","potatos","cabbages"]
+// console.log(shoppinglist);
+// shoppinglist.push("juice","bread")
+// console.log(shoppinglist);
+// shoppinglist.pop()
+// console.log(shoppinglist);
+// shoppinglist.shift()
+// shoppinglist.unshift("eggs")
+// console.log(shoppinglist);
+// console.log(shoppinglist[3]);
+// shoppinglist.splice(3,1)
+// console.log(shoppinglist)
+// let fruits=["appricot","grapes"]
+// shoppinglist[5]=fruits
+// console.log(shoppinglist)
+// now in order o find the index no. of a particular element.
+// console.log(shoppinglist.indexOf("strawberry"));
+// shoppinglist.reverse()
+// shoppinglist.sort()
+
+// console.log(shoppinglist)
+let list=[]
+console.log(list)
+list.push("apple","banana","cherry");
+console.log(list)
+// list.pop()
+// console.log(list)
+// list.unshift("mango")
+// console.log(list)
+// list.shift()
+// console.log(list)
+// list.sort()
+// console.log(list)
+// list.reverse()
+// console.log(list)
+// console.log(list.indexOf("banana"))
+let fruit=["grape"]
+list[2]=fruit
+console.log(list)
+// let numbers=["1","2","3","4","5","6","7","8","9","10"]
+// console.log(number)
+// number.splice(4,3)
+// console.log(number)
+
