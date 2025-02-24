f754a87b3086faf3f1f4cb8cc3cba239a8a22d80
rubina75 <www.khanruby49@gmail.com>
2025-02-24 17:42:33 +0500

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,31 @@
+// for(let i=0; i<15; i++){
+//     console.log('hello');
+// }
+// for(let i=15; i>0; i--){
+//     console.log('hello');
+// }
+for(let i=1; i<=15; i++){
+    console.log(i);
+}
+for(let i=10; i>0; i--){
+    console.log(i);
+}
+
+let fruits=['banana','cheery','mango','apple']
+for (let i=0; i<fruits.length; i++){
+    console.log(`${fruits[i]} is available`);
+}
+
+let i=0;
+while(i<5){
+    console.log('hello');
+    
+
+}
+// tasks
+num=[30,80,20,10,30]
+let sum=0;
+for(i=0; i<num.length;i++){
+sum=sum+num[i];
+}
+console.log(sum);
